693ce73eaa00a894bae38ec10e7f28a1107e1fb0
hannahf86 <hannahfeehan.design@gmail.com>
2023-08-07 18:00:46 +0100

    invoice page complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SideBar from './Global/SideBar';
 
 import Dashboard from './Scenes/Dashboard';
 import Team from './Scenes/Team';
-// import Invoices from './Scenes/Invoices';
+import Invoices from './Scenes/Invoices';
 import Contacts from './Scenes/Contacts';
 // import Bar from './Scenes/Bar';
 // import Form from './Scenes/Form';
@@ -34,7 +34,7 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
