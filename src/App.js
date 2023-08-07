47dda939929effb731ffc6155d179b0119f8bbc1
hannahf86 <hannahfeehan.design@gmail.com>
2023-08-07 18:45:18 +0100

    Form completed, dependancies updated

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import Team from './Scenes/Team';
 import Invoices from './Scenes/Invoices';
 import Contacts from './Scenes/Contacts';
 // import Bar from './Scenes/Bar';
-// import Form from './Scenes/Form';
+import Form from './Scenes/Form';
 // import Line from './Scenes/Line';
 // import Pie from './Scenes/Pie';
 // import FAQ from './Scenes/FAQ';
 // import Geography from './Scenes/Geography';
-// import Calendar from './Scenes/Calendar';
+import Calendar from './Scenes/Calendar';
 
 
 function App() {
@@ -35,12 +35,13 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} />   */}
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
 
 
---
+++ src/Scenes/Calendar/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import { Box, List, ListItem, ListItemText, Typography, useTheme } from '@mui/icons-material';
+import Header from "../../Components/Header";
+import { tokens } from "../../Themes";
+
+const Calendar = () => {
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode);
+    const [currentEvents, setCurrentEvents] = useState([]);
+
+    const handleDataClick = (selected) => {
+        const title = prompt("Please enter a new title for your event");
+        const calendarApi = selected.view.calendar;
+        calendarApi.unselect();
+
+        if (title) {
+            calendarApi.addEvent({
+                id: `${selected.dateStr}-${title}`,
+                title,
+                start: selected.startStr,
+                end: selected.endStr,
+                allDay: selected.allDay,
+            })
+        }
+    }
+}
+
+
+
+
+export default Calendar;
