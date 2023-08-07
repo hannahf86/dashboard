bfa5846210584eb0d27593ae31b7376db1715c64
hannahf86 <hannahfeehan.design@gmail.com>
2023-08-07 14:34:11 +0100

    Sidebar complete

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { ColorModeContext, useMode } from "./Themes";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from 'react-router-dom'
 import TopBar from "./Global/TopBar"
-import Dashboard from './Scenes/Dashboard';
 import SideBar from './Global/SideBar';
 
+// import Dashboard from './Scenes/Dashboard';
 // import Team from './Scenes/Team';
 // import Invoices from './Scenes/Invoices';
 // import Contacts from './Scenes/Contacts';
@@ -27,11 +27,12 @@ function App() {
         <CssBaseline />
 
         <div className="app">
+          <SideBar />
           <main className="content">
             <TopBar />
             <Routes>
-              {/* <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
+              {/* <Route path="/" element={<Dashboard />} /> */}
+              {/* <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/bar" element={<Bar />} />
@@ -39,7 +40,7 @@ function App() {
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/calendar" element={<Calendar />} />  */}
             </Routes>
 
 
